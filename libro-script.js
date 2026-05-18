dd8fb5c15f4f97fdb26afd7c89b729b3f0ad6bf7
Richard Wong <154770346+RichardWong2004@users.noreply.github.com>
2026-05-18 10:11:07 -0500

    Update libro-script.js

--- libro-script.js
+++ libro-script.js
@@ -183,10 +183,10 @@ function generateHeart() {
     heartCont.style.visibility = 'visible';
 
     const photos = [
-    '../imagenes/h.png',
-    '../imagenes/nao3.png',
-    '../imagenes/corazon.png'
-    ];
+    'imagenes/h.png',
+    'imagenes/nao3.png',
+    'imagenes/corazon.png'
+];
 
     const totalPhotos = 22;
     const scaleFactor = 14;
